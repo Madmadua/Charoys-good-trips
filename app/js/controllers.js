ff762e13beea5a8d4056a4ca949248f14cd15bd4
vanessadegre <vanessa.degre@telecomnancy.net>
2013-11-28 17:09:28 +0100

    affichage hotels

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,12 +4,12 @@
 
 var cgtControllers = angular.module('cgtControllers', []);
 
-cgtControllers.controller('cgtCtrl', ['$scope', '$http', 
+cgtControllers.controller('cgtCtrl', ['$scope', '$http',
 	function($scope, $http) {
 
 
 		$http.post('/proxy', {'url': 'http://api.outpost.travel/placeRentals?city=Paris'}).success(function(data) {
-			$scope.items = data.items;
+			$scope.items = data.items.splice(0,5);
 		});
 		
 
