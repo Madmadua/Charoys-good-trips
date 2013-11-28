016de61b8fa3ecb1ec5ad0cbb3b61f24dfb725e7
platane <arthur@arthur-brongniart.fr>
2013-11-28 17:46:25 +0100

    my bad

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,18 +6,6 @@ var cgtControllers = angular.module('cgtControllers' , []);
 
 cgtControllers.controller('cgtCtrl', ['$scope', '$http',
 	function($scope, $http) {
-		
-	    $http({method: 'GET', url: 'items.json'}).
-	    success(function(data, status, headers, config) {
-	    	console.log(data);
-
-	    }).
-	    error(function(data, status, headers, config) {
-	    	console.log(data);
-	    	console.log(status);
-	    	console.log(headers);
-	    	console.log(config);
-	    });
 
 	    $scope.search = function() {
 			$http.post('/proxy', {'url': 'http://api.outpost.travel/placeRentals?city=' + $scope.search_location}).success(function(data) {
