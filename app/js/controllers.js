8492e000b69e7810cc095fe9225d36031141851d
Cedric Grard <Cedric.grard@telecomnancy.net>
2013-11-28 18:22:44 +0100

    Interface

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,6 +11,10 @@ cgtControllers.controller('cgtCtrl', ['$scope', '$http',
 		$scope.search = function() {
 			$http.post('/proxy', {'url': 'http://api.outpost.travel/placeRentals?city=' + $scope.search_location}).success(function(data) {
 				$scope.items = data.items.splice(0,5);
+				/*$scope.items.forEach(function(item) {
+			      alert(item);
+			      item.latLng = item.latLng.replace('[', "(");
+			    });*/
 			});
 		};
 	}
