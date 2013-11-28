ef1f2152bf968f1f1dc1982e5e371e6b94b01505
Cedric Grard <Cedric.grard@telecomnancy.net>
2013-11-28 13:03:38 +0100

    Ajout d'un service pour SocketIO

--- app/js/services.js
+++ app/js/services.js
@@ -2,3 +2,37 @@
 
 /* Services */
 
+var cgtServices = angular.module('cgtServices', []);
+
+
+/*
+ * Socket
+ *
+ * https://github.com/btford/angular-socket-io-seed
+ */
+cgtServices.factory('socket', function($rootScope, $timeout) {
+    var socket = io.connect();
+
+    return {
+        on: function(eventName, callback) {
+            socket.on(eventName, function() {
+                var args = arguments;
+
+                $timeout(function() {
+                    callback.apply(socket, args);
+                }, 0);
+            });
+        },
+        emit: function(eventName, data, callback) {
+            socket.emit(eventName, data, function() {
+                var args = arguments;
+
+                $rootScope.$apply(function() {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            });
+        }
+    };
+});
